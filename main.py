99990dec5c4e874d3c2cff88c98fcd4e5a93a0d3
Alessio Galliazzo <agalliazzo@gmail.com>
2022-08-16 16:43:14 +0200

    Initial commit with a basic working version

---
+++ main.py
@@ -0,0 +1,505 @@
+import dataclasses
+import logging
+import sys
+import os.path
+import random
+import time
+import xml.etree.ElementTree
+from socket import socket, AF_INET, SOCK_STREAM
+import ssl
+from ssl import SSLContext, SSLError
+from ssdpy import SSDPClient, SSDPServer
+from logging import info, warning, error, basicConfig
+from pprint import pprint
+from threading import Thread, Event
+from xml.etree import ElementTree
+from paho.mqtt import client
+from dataclasses import dataclass
+from typing import Callable
+from abc import abstractmethod, ABC, abstractproperty
+from paho import mqtt
+import asyncio
+import socket
+from samsung_discovery import SamsungDiscovery
+import json
+import argparse
+
+basicConfig(level='INFO')
+
+# La password del wifi SMARTAIRCON è JUNGFRAU2011 non 111112222 come scritto nel manuale
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+@dataclass
+class SamsungAirConditioner:
+    mac_address: str
+    ip_address: str
+    nickname: str
+    token: str
+
+
+class SamsungAirConditionerConfigParser(dict[str, SamsungAirConditioner]):
+    def __init__(self, config_file: str = 'config.json'):
+        self._config_file = config_file
+        if not os.path.exists(config_file):
+            return
+
+        with open(config_file, 'r') as f:
+            raw_config = f.read()
+            if raw_config == '':
+                return
+            try:
+                acs = json.loads(raw_config)
+            except Exception as e:
+                print(e)
+                return
+            for mac, ac in acs.items():
+                self[mac] = (SamsungAirConditioner(**ac))
+
+    def save(self):
+        with open(self._config_file, 'w') as f:
+            f.write(json.dumps(self, cls=EnhancedJSONEncoder))
+
+
+
+@dataclass
+class SamsungACProperty:
+    samsung_property: str
+    access_type: str = 'RW'
+    data_type: type = str
+    value: str = ''
+    mqtt_name: str = None
+
+    def __post_init__(self):
+        self.mqtt_name = self.mqtt_name or self.samsung_property
+
+
+class SamsungACPropertyList(dict[SamsungACProperty]):
+    """
+    Manage the properties for the air conditioner.
+    """
+    def __init__(self, mqtt_server: str, prop_changed_callback: Callable = None, base_topic: str = '/AC/'):
+        self._base_topic: str = base_topic if base_topic[-1] != '/' else base_topic[:-1]
+        self._mqtt_client: mqtt.client.Client = mqtt.client.Client()
+        self._mqtt_server: str = mqtt_server
+        self._prop_changed_callback: Callable = prop_changed_callback
+
+    def mqtt_connect(self):
+        self._mqtt_client.on_connect = self._on_mqtt_connect
+        self._mqtt_client.on_disconnect = self._on_mqtt_disconnect
+        self._mqtt_client.on_message = self._on_mqtt_message
+        self._mqtt_client.connect(self._mqtt_server)
+        self._mqtt_client.loop_start()
+
+    def _on_mqtt_connect(self, client, userdata, flags, rc):
+        for topic in self.get_subscription_list():
+            self._mqtt_client.subscribe(topic)
+
+    def _on_mqtt_disconnect(self, client, userdata, flags, rc):
+        pass
+
+    def _on_mqtt_message(self, client, userdata, msg):
+        topic_splitted = msg.topic.split('/')
+        property_id = topic_splitted[-1]
+        property = self.get(property_id, None)
+        if property is not None:
+            property.value = msg.payload.decode('utf-8')
+            if self._prop_changed_callback:
+                self._prop_changed_callback(property)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, SamsungACProperty):
+            key = key.samsung_property
+
+        already_exists = True if key in self.keys() else False
+
+        if isinstance(value, SamsungACProperty):
+            super(SamsungACPropertyList, self).__setitem__(key, value)
+            obj: SamsungACProperty = self[key]
+        else:
+            obj: SamsungACProperty = self[key]
+            obj.value = value
+            print('Publishing %s to topic %s' % (obj.value, self._get_full_topic('Reply', obj)))
+            self._mqtt_client.publish(self._get_full_topic('Reply', obj), obj.value)
+
+        if not already_exists and self._mqtt_client.is_connected():
+            self._mqtt_client.subscribe(self._get_full_topic(obj))
+
+    def __getitem__(self, item):
+        return super(SamsungACPropertyList, self).__getitem__(item)
+    
+    def __iadd__(self, other: SamsungACProperty):
+        self[other.samsung_property] = other
+        return self
+
+    def _get_full_topic(self, command, obj: SamsungACProperty):
+        return '%s/%s/%s' % (self._base_topic, command, obj.mqtt_name)
+
+    def get_subscription_list(self):
+        return [self._get_full_topic('Set', k[1]) for k in self.items()]
+
+
+class SamsungAC:
+    """
+    General class to connect to an air conditioner
+    """
+    convenient_modes = ['Off', 'Quiet', 'Sleep', 'Smart', 'SoftCool', 'TurboMode', 'WindMode1', 'WindMode2',
+                        'WindMode3']
+    work_modes = ['Auto', 'Cool', 'Dry', 'Wind', 'Heat']
+    wind_modes = ['Auto', 'Low', 'Mid', 'High', 'Turbo']
+
+    def __init__(self, ip_address: str, duid, token='', base_topic: str = '/AC/', friendly_name: str = None,
+                 mqtt_broker: str = 'mqtt.agalliazzo.com'):
+        """
+        Default initializer
+        :param ip_address: Address of the air conditioner
+        :param duid: Device Unique ID (MAC address) of the air conditioner
+        :param token: Auth token if already obtained
+        :param base_topic: Base topic for publishing data to the MQTT broker
+        :param friendly_name: Friendly name of the AC (e.g. DinningRoomAC, RestRoomAC, BedRoomAC). This is added after
+        base_topic to complete the topic for the device
+        :param mqtt_broker: MQTT broker address (or FQDN) where to publish and subscribe
+        """
+        # Network interface
+        self.IPAddress = ip_address
+        self.port = 2878
+        self.socket = None
+        self.ssl_socket = None
+        self.ssl_context = None
+        self.IsConnected = False
+        self.duid = duid
+        self.token = token
+        friendly_name = friendly_name or duid
+        base_topic = base_topic if base_topic[:-1] == '/' else base_topic + '/'
+        self.properties = SamsungACPropertyList(mqtt_broker, self.mqtt_prop_change_request,
+                                                base_topic + friendly_name)
+        self.properties += SamsungACProperty('AC_FUN_ENABLE', 'RW')
+        self.properties += SamsungACProperty('AC_FUN_POWER')
+        self.properties += SamsungACProperty('AC_FUN_SUPPORTED')
+        self.properties += SamsungACProperty('AC_FUN_OPMODE')
+        self.properties += SamsungACProperty('AC_FUN_TEMPSET')
+        self.properties += SamsungACProperty('AC_FUN_COMODE')
+        self.properties += SamsungACProperty('AC_FUN_ERROR')
+        self.properties += SamsungACProperty('AC_FUN_TEMPNOW')
+        self.properties += SamsungACProperty('AC_FUN_SLEEP')
+        self.properties += SamsungACProperty('AC_FUN_WINDLEVEL')
+        self.properties += SamsungACProperty('AC_FUN_DIRECTION')
+        self.properties += SamsungACProperty('AC_ADD_AUTOCLEAN')
+        self.properties += SamsungACProperty('AC_ADD_APMODE_END')
+        self.properties += SamsungACProperty('AC_ADD_STARTWPS')
+        self.properties += SamsungACProperty('AC_ADD_SPI')
+        self.properties += SamsungACProperty('AC_SG_WIFI')
+        self.properties += SamsungACProperty('AC_SG_INTERNET')
+        self.properties += SamsungACProperty('AC_ADD2_VERSION')
+        self.properties += SamsungACProperty('AC_SG_MACHIGH')
+        self.properties += SamsungACProperty('AC_SG_MACMID')
+        self.properties += SamsungACProperty('AC_SG_MACLOW')
+        self.properties += SamsungACProperty('AC_SG_VENDER01')
+        self.properties += SamsungACProperty('AC_SG_VENDER02')
+        self.properties += SamsungACProperty('AC_SG_VENDER03')
+        self.properties.mqtt_connect()
+        # Listener
+        self.exitThread = Event()
+        self.listeningThread = Thread(target=self.listener)
+        self.on_token_received: Callable = None
+
+    def mqtt_prop_change_request(self, prop: SamsungACProperty):
+        info("MQTT prop change requested for property %s" % prop)
+        self._device_control(prop.samsung_property, prop.value)
+
+    def check_resp(self, element, resp):
+        if element.attrib['Type'] == resp and element.attrib['Status'] == 'Okay':
+            return True
+        return False
+
+    def listener(self):
+        """
+        Listener thread to read socket data
+        TODO: Use select to poll the socket, this is blocking and is not beautiful
+        :return:
+        """
+        while not self.exitThread.is_set():
+            data = self.ssl_socket.read(len=2048)
+            if b'DRC-1.00' in data:
+                continue
+            logging.debug("Received: %s" % data)
+            tree = ElementTree.ElementTree(ElementTree.fromstring(data))
+            root = tree.getroot()
+            if root.tag == 'Update':
+                if root.attrib['Type'] == 'InvalidateAccount':
+                    logging.info("Not yet authenticated... proceeding with login...")
+                    self.login()
+                else:
+                    # TODO: Check how to merge in a whole function, something is strange in here about how I wrote this
+                    self.parse_update(root)
+
+            if root.tag == 'Response':
+                if self.check_resp(root, 'AuthToken'):
+                    #login sucessfull
+                    self.get_status()
+
+                if self.check_resp(root, 'DeviceState'):
+                    self.parse_status(root)
+
+    def parse_update(self, el: xml.etree.ElementTree.Element):
+        """
+        Process and update command from the air conditioner
+        :param el: xml received element
+        :return:
+        """
+        type = el.attrib.get('Type', None)
+        if type == 'GetToken':
+            token = el.attrib.get('Token')
+            print("Token ricevuto: %s" % token)
+            if self.on_token_received:
+                self.on_token_received(self.duid, token)
+            return
+
+        for child in el.find('Status'):
+            if child.tag == 'Attr':
+                self.properties[child.attrib['ID']] = child.attrib['Value']
+
+    def parse_status(self, el: xml.etree.ElementTree.Element):
+        """
+        Process a DeviceStatus "command" from the air conditioner
+        :param el: xml received element
+        :return:
+        """
+        for child in el.find('DeviceState').find('Device'):
+            if child.tag == 'Attr':
+                print(child.attrib['ID'], child.attrib['Value'], child.attrib['Type'])
+                self.properties[child.attrib['ID']] = child.attrib['Value']
+
+    def _send(self, data: bytes or str):
+        """
+        Send data to air conditioner
+        :param data: data to be sent
+        :return:
+        """
+        if isinstance(data, str):
+            data = str.encode(data)
+        info("Sending: %s" % data)
+        self.ssl_socket.write(data + b"\r\n")
+
+    def _device_control(self, key, value):
+        """
+        Execute a change property request on the air conditioner
+        :param key: property to change
+        :param value: new value
+        :return:
+        """
+        id = random.randint(0, 10000)
+        self._send(
+            '<Request Type="DeviceControl"><Control CommandID="cmd%s" DUID="%s"><Attr ID="%s" Value="%s" /></Control></Request>' %
+            (id, self.duid, key, value)
+        )
+
+    def connect(self):
+        """
+        Connect to the air conditioner
+        :return:
+        """
+        if self.IsConnected:
+            return
+
+        self.socket = socket.socket(AF_INET, SOCK_STREAM)
+        self.ssl_context = SSLContext(protocol=ssl.PROTOCOL_SSLv23)
+        self.ssl_context.options &= ~(ssl.OP_NO_SSLv3 | ssl.OP_NO_SSLv2)
+        self.ssl_socket = ssl.wrap_socket(self.socket, ssl_version=ssl.PROTOCOL_TLSv1, ciphers='DEFAULT:!DH')
+        try:
+            self.ssl_socket.connect((self.IPAddress, self.port))
+            self.listeningThread.start()
+            info("Connected to %s" % self.IPAddress)
+            self.IsConnected = True
+            return
+        except SSLError as err:
+            error(err)
+        self.IsConnected = False
+
+    def get_token(self):
+        """
+        Start the authorization process.
+        This mean:
+        - Ask to the air conditioner to issue a Token
+        - Turn on the air conditioner through the remote controller to confirm the token generation (if AC is on tunr off and on again)
+        - On answer from AC self.on_token_received is called with the MAC of the AC and the token received
+        :return:
+        """
+        self.connect()
+        self._send(b'<Request Type="GetToken"/>')
+        info('Request sent')
+
+    def login(self):
+        """
+        Login to the AC using the received token
+        :return:
+        """
+        self._send(b'<Request Type="AuthToken"><User Token="%s" /></Request>' % str.encode(self.token))
+
+    def get_status(self):
+        """
+        Get the AC current status (list all the properties)
+        :return:
+        """
+        self._send('<Request Type="DeviceState" DUID="%s"></Request>' %  self.duid )
+
+    def turn_on(self):
+        """
+        Turn on the AC
+        :return:
+        """
+        self._device_control('AC_FUN_POWER', 'On')
+
+    def turn_off(self):
+        """
+        Turn off the AC
+        :return:
+        """
+        self._device_control('AC_FUN_POWER', 'Off')
+
+    def set_temperature(self, target: int):
+        """
+        Set the target temperature
+        :param target: Target temperature
+        :return:
+        """
+        assert isinstance(target, int)
+        self._device_control('AC_FUN_TEMPSET', target)
+
+    def get_temperature(self):
+        """
+        Get the current temperature setpoint
+        :return:
+        """
+        return self.properties['AC_FUN_TEMPSET']
+
+    def set_windmode(self, mode: int or str):
+        """
+        Set the fan level
+        :param mode:
+        :return:
+        """
+        if isinstance(mode, int):
+            mode = self.wind_modes[mode]
+        self._device_control('AC_FUN_WINDLEVEL', mode)
+
+    def get_actual_temperature(self):
+        """
+        Read the actual room temperature
+        :return:
+        """
+        return self.properties['AC_FUN_TEMNOW']
+
+
+def update_configured_devices(configured_devices: SamsungAirConditionerConfigParser):
+    """
+    Start a discovery process using SSDP and update the configuration file with the addresses of the air conditioners
+    TODO: This process is blocking. Maybe we can use a non-blocking style
+    :param configured_devices: instance of SamsungAirConditionerConfigParser
+    :return:
+    """
+    discovery = SamsungDiscovery()
+    while not discovery.discovery_finished:
+        time.sleep(1)
+    for mac, ipaddr in discovery.discovered_items.items():
+        device = configured_devices.get(mac, None)
+        if device is None:
+            device = SamsungAirConditioner(mac_address=mac, ip_address=ipaddr, nickname='', token='')
+        configured_devices[mac] = device
+    configured_devices.save()
+
+
+configured_devices = SamsungAirConditionerConfigParser()
+
+
+def on_token_received(ac, token):
+    """
+    Callback to update the configured devices once the token is received
+    :param ac: MAC address (index of the configuration dict) for the air conditioner
+    :param token: Token received
+    :return:
+    """
+    configured_devices[ac].token = token
+    configured_devices.save()
+
+
+def interactive():
+    """
+    Interactive shell to run configuration stuff and testing
+    :return:
+    """
+    print('Total device in conf file: %s' % len(configured_devices))
+
+    acs = {}
+    for mac, device in configured_devices.items():
+        acs[device.nickname] = SamsungAC(ip_address=device.ip_address, duid=device.mac_address, token=device.token,
+                                         base_topic='/domoticV2/%s' % device.nickname, friendly_name=device.nickname)
+        acs[device.nickname].on_token_received = on_token_received
+        ac = acs[device.nickname]
+
+    r = ''
+    while r != 'q':
+        print('s: Select AC')
+        print('d: Start discovery of ACs')
+        print('1: Connect')
+        print('2: Turn off')
+        print('3: Turn on')
+        print('4: get status')
+        print('5: get token')
+        print('6: set temperature')
+        print('7: set fan')
+        print('q: Exit')
+        r = input('Choose the command: ')
+        if r == 's':
+            pprint('Available ACs: %s' % acs.keys())
+            ac_name = ''
+            while ac_name not in acs.keys():
+                ac_name = input('Select: ')
+            ac = acs[ac_name]
+        if r == 'd':
+            update_configured_devices(configured_devices, acs)
+            for mac, device in configured_devices.items():
+                acs[device.nickname] = SamsungAC(ip_address=device.ip_address, duid=device.mac_address, token=device.token,
+                                                 base_topic='/domoticV2/%s' % device.nickname,
+                                                 friendly_name=device.nickname)
+                ac = acs[device.nickname]
+        if r == '1':
+            ac.connect()
+            info("Connected")
+        if r == '2':
+            ac.turn_off()
+        if r == '3':
+            ac.turn_on()
+        if r == '4':
+            ac.get_status()
+        if r == '5':
+            ac.get_token()
+        if r == '6':
+            r = input('Which temperature? ')
+            ac.set_temperature(int(r))
+
+        if r == '7':
+            r = input('Which fan (%s)? ' % SamsungAC.wind_modes)
+            ac.set_windmode(r)
+
+
+def automatic_run():
+    """
+    Automatically start the system, connect to the air conditioner and start mqtt client
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    interactive()
+
+
+    #parser = argparse.ArgumentParser(description='Samsung AC old mqtt interface (based on port 2787)')
+    #parser.add_argument('-i', dest='')
+
---
+++ samsung_discovery.py
@@ -0,0 +1,67 @@
+import datetime
+import pprint
+import socket
+import struct
+import time
+from threading import Thread
+import select
+
+class SamsungDiscovery:
+    MCAST_GRP = '239.255.255.250'
+    MCAST_PORT = 1900
+    MULTICAST_TTL = 5
+
+    def __init__(self, mcast_grp='192.168.2.255', ttl=5):
+        self.discovered_items = {}
+        self.MCAST_GRP = mcast_grp
+        self.MULTICAST_TTL = ttl
+        self._recv_thread = Thread(target=self.receive)
+        self._recv_thread.start()
+        time.sleep(1)
+        self.send_request()
+        self.discovery_finished = False
+
+    def send_request(self):
+        data = b'''NOTIFY * HTTP/1.1\r
+HOST: 239.255.255.250:1900\r
+CACHE-CONTROL: max-age=20\r
+SERVER: AIR CONDITIONER\r
+\r
+SPEC_VER: MSpec-1.00\r
+SERVICE_NAME: ControlServer-MLib\r
+MESSAGE_TYPE: CONTROLLER_START\r
+'''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.MULTICAST_TTL)
+        sock.sendto(data, (self.MCAST_GRP, self.MCAST_PORT))
+
+    def receive(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', self.MCAST_PORT))
+        mreq = struct.pack("4sl", socket.inet_aton('239.255.255.250'), socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        start_dt = datetime.datetime.now()
+
+        sock.setblocking(False)
+        while (datetime.datetime.now() - start_dt) < datetime.timedelta(seconds=5):
+            ready = select.select([sock], [], [], 1)
+            if ready[0]:
+                msg = sock.recv(10240)
+                msgs = msg.decode('utf-8').split('\r\n')
+                properties = {}
+                for m in msgs:
+                    colon_pos = m.find(':')
+                    key = m[:colon_pos].strip()
+                    value = m[colon_pos+1:].strip()
+                    properties[key] = value
+
+                if 'SERVER' not in properties.keys():
+                    continue
+
+                if 'SAMSUNG-AC' in properties['SERVER']:
+                    print("Found AC at %s with ID %s" % (properties['LOCATION'][7:], properties['MAC_ADDR']))
+                    if properties['MAC_ADDR'] in self.discovered_items.keys():
+                        continue
+                    self.discovered_items[properties['MAC_ADDR']] = properties['LOCATION'][7:]
+        self.discovery_finished = True
